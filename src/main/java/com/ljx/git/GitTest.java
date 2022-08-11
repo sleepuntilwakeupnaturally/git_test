037ebdede32c4f9c2757c1746fb9a0bf066c0ca9
ljx <ljx@linjianxi.com>
2022-08-11 20:58:08 +0800

    first commit

---
+++ src/main/java/com/ljx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ljx.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
