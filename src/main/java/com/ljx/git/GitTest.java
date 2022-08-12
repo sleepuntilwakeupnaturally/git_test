37ca69d3b29a8d24a78067e0c9a671cd01c57ad2
ljx <ljx@linjianxi.com>
2022-08-12 18:24:53 +0800

    push commit1

--- src/main/java/com/ljx/git/GitTest.java
+++ src/main/java/com/ljx/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello master");
         System.out.println("hello teat");
+        System.out.println("push test");
     }
 
 }
